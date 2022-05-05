8da1ff5abe3fd52dc8cbee7ff8a51ca165d38fa7
Adam Yiu <adam_yiu@outlook.com>
2022-05-05 20:42:37 +0000

    Implement takeUntil (W1D4)

---
+++ takeUntil.js
@@ -0,0 +1,11 @@
+const takeUntil = function (array, callback) {
+  for (let i = 0; i< array.length; i++) {
+    if (callback(array[i]))
+    return array.slice(0, i )
+  }
+  return array;
+}
+
+const data1 = [1, 2, 5, 7, -2, 1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+console.log(results1);
