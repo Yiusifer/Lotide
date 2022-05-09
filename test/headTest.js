b950443c46f3195bd9f52a7aa0bee4f630ec419c
Adam Yiu <adam_yiu@outlook.com>
2022-05-09 21:51:43 +0000

    Using Mocha and Chai to add test cases to head.js/headTest.js

--- test/headTest.js
+++ test/headTest.js
@@ -1,4 +1,26 @@
+// Importing relevant functions
+const { assert } = require("chai");
 const assertEqual = require("../assertEqual");
+const head = require('../head');
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+// Test cases
+describe ('#head', () => {
+  it ('returns 1 for [1, 2, 3]', () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+
+  it('should return 5 for [5]', () => {
+    assert.strictEqual(head([5]), 5 );
+  })
+
+  it ('returns 5 for [5, 6, 7]', () => {
+    assert.strictEqual(head([5, 6, 7]), 5);
+  });
+
+  it ('returns Hello for ["Hello", "Lighthouse", "Labs"]', () => {
+    assert.strictEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+  });
+
+
+
+});
