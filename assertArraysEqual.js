426d46b9bd6300edcbf392b89124d7eb8376f6f2
Adam Yiu <adam_yiu@outlook.com>
2022-05-09 20:49:16 +0000

    Refractored assertArraysEqualTest and seperated test cases

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,26 +1,18 @@
-const assertEqual = function (actual, expected) {
-  if (actual === expected) {
-    console.log(`✅ ✅ ✅ Assertion passed: ${actual} === ${expected}`);
-  } else {
-    console.log("⛔ ⛔ ⛔ Assertion failed: " + actual + " !== " + expected);
-  }
-
-};
 
 const assertArraysEquals = function (arrayOne, arrayTwo) {
   if (arrayOne.length !== arrayTwo.length) {
-    console.log("⛔ ⛔ ⛔ Assertion failed: " + arrayOne + " !== " + arrayTwo);
+    return ("⛔ ⛔ ⛔ Assertion failed: " + arrayOne + " !== " + arrayTwo);
+  }
 
-    for (let i = 0; i < arrayOne.length; i++) {
-      if (arrayOne[i] !== arrayTwo[i]) {
-        console.log("⛔ ⛔ ⛔ Assertion failed: " + arrayOne + " !== " + arrayTwo);
-        break;
-
-      } else {
-        console.log(`✅ ✅ ✅ Assertion passed: ${actual} === ${expected}`)
-      }
-
+  for (let i = 0; i < arrayOne.length; i++) {
+    if (arrayOne[i] !== arrayTwo[i]) {
+      return ("⛔ ⛔ ⛔ Assertion failed: " + arrayOne + " !== " + arrayTwo);
     }
   }
+  return (`✅ ✅ ✅ Assertion passed: ${actual} === ${expected}`);
 }
-assertArraysEquals([1, 2, 3], [2])
+
+
+// Exporting assertArraysEquals function
+
+module.exports = assertArraysEquals;
---
+++ test/assertArraysEqualTest.js
@@ -0,0 +1,12 @@
+// Importing eqArrays function
+const eqArrays = require('../eqArrays');
+
+// Importing assertArraysEquals function
+const assertArraysEquals = require('../assertArraysEqual');
+
+
+
+
+// Test case
+console.log(assertArraysEquals([1, 2, 3], [2]));
+
